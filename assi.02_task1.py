f6261365673a753db469717c236bceb10be3d572
Mo Alam <akhan398635@gmail.com>
2025-06-17 16:43:03 +0530

    Add files via upload

---
+++ assi.02_task1.py
@@ -0,0 +1,20 @@
+a =int(input("Enter the code:"))
+if a %2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
+print("Thank you")
+
+
+'''
+output:- for odd
+Enter the code:3
+Number is odd
+Thank you
+'''
+'''
+outpot:- for even
+Enter the code:6
+Number is even
+Thank you
+'''
---
+++ assi.02_task2.py
@@ -0,0 +1,9 @@
+total_sum=0
+for num_dig in range(1,51):
+    total_sum = total_sum + num_dig
+    print("sum of all interger from 1 to 50 is: ", total_sum)
+
+
+'''output:-
+sum of all interger from 1 to 50  1275
+'''
